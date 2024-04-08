677f9f778f8c8b220b3a7f0f58b5c95b3fb4c95a
eabell <cjp975@naver.com>
2024-04-08 16:40:47 +0900

    test

--- Jenkinsfile.scm
+++ Jenkinsfile.scm
@@ -1,13 +1,12 @@
-pipeline{
-    agent{
-        label "node"
-    }
-    stages{
-        stage("A"){
-            steps{
-                echo "========executing A========"
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                    credentialsId: 'rayful',
+                    url: 'https://github.com/Ea-Bell/testGItJenkinsTest.git'
             }
         }
     }
-
 }
