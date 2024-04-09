900c4822f3a811fb69603ab84d69e5c9a290befe
eabell <cjp975@naver.com>
2024-04-09 16:24:49 +0900

    gdgsdg

--- Jenkinsfile.scm
+++ Jenkinsfile.scm
@@ -1,12 +1,14 @@
 pipeline {
     agent any
     stages {
-        stage('gitHubHook') {
+        stage('ready') {
             steps {
                 git branch: 'main',
                     credentialsId: 'rayful',
                     url: 'https://github.com/Ea-Bell/testGItJenkinsTest.git'
             }
+        }stage('build'){
+            git clone 'https://github.com/Ea-Bell/testGItJenkinsTest.git'
         }
     }
 }
