0ebf455a0178cd790ea565229f4fabaa2f9f158a
eabell <cjp975@naver.com>
2024-04-09 10:30:27 +0900

    test

--- Jenkinsfile.scm
+++ Jenkinsfile.scm
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     stages {
-        stage('Checkout') {
+        stage('gitHubHook') {
             steps {
                 git branch: 'main',
                     credentialsId: 'rayful',
