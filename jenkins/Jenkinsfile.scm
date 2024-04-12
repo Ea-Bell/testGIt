0e5f7713e21205296b966ff43e76fe01a4527d21
eabell <cjp975@naver.com>
2024-04-12 10:13:49 +0900

    젠킨스 테스트

--- jenkins/Jenkinsfile.scm
+++ jenkins/Jenkinsfile.scm
@@ -32,35 +32,14 @@ pipeline {
                 }
             }
         }
-        // stage('SSH transfer') {
-        //     steps{
-        //         script {
-        //             sshPublisher(
-        //             continueOnError: false, failOnError: true,
-        //             publishers: [
-        //                 sshPublisherDesc(
-        //                 configName: "${env.SSH_CONFIG_NAME}",
-        //                 verbose: true,
-        //                 transfers: [
-        //                     sshTransfer(
-        //                     sourceFiles: "my-app/build/*", // 전송할 파일 경로 설정
-        //                     removePrefix: "my-app/build", // 제거할 접두사 설정 (옵션)
-        //                     remoteDirectory: "/home/EaBell/temp", // 원격 디렉토리 경로 설정
-        //                     execCommand: "run commands after copy?"
-        //                     )
-        //                 ])
-        //             ])
-        //         }
-        //     }
-        // }
-        // stage('send buildFile'){
-        //     steps{
-        //         echo 'send builFile jenkins -> targetServer'
-        //         dir('my-app'){
-        //             // bat 'sftp build EaBell@192.168.10.173:~/temp'
-        //             bat 'ssh EaBell@192.168.10.173 "chmod -R 755 /home/EaBell/temp/build"'
-        //         }
-        //     }
-        // }
+        stage('send buildFile'){
+            steps{
+                echo 'send builFile jenkins -> targetServer'
+                dir('my-app'){
+                    bat 'scp build EaBell@192.168.10.173:~/temp'
+                    bat 'ssh EaBell@192.168.10.173 "chmod -R 755 /home/EaBell/temp/build"'
+                }
+            }
+        }
     }
 }
