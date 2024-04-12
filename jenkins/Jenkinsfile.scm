d4b4e9216b5eb3877ea4333abe681d76e88e26fc
eabell <cjp975@naver.com>
2024-04-12 11:43:15 +0900

    Test

--- jenkins/Jenkinsfile.scm
+++ jenkins/Jenkinsfile.scm
@@ -5,7 +5,7 @@ pipeline {
         JENKINS_CREDENTIALSID= 'rayful'
         GIT_URL = 'https://github.com/Ea-Bell/testGItJenkinsTest.git'
         GIT_BRANCH= 'main'
-        target_ip='192.168.10.173'
+        TARGET_IP='192.168.10.173'
         TARGET_ID='EaBell'
         TARGET_BUILD_FILEPATH='/home/EaBell/temp/build' 
     }
@@ -30,11 +30,11 @@ pipeline {
             steps {
                 echo "send buildFile jenkins -> targetServer"
                 dir('my-app') {
-                    bat '''
-                        ssh ${TARGET_ID}@${target_ip} "rm -rf ${TARGET_BUILD_FILEPATH}"
-                        scp -r build ${target_ip}@${target_ip}:${TARGET_BUILD_FILEPATH}
-                        ssh ${TARGET_ID}@${target_ip} "chmod -R 755 ${TARGET_BUILD_FILEPATH}"
-                        '''
+                    bat """
+                        ssh ${TARGET_ID}@${TARGET_IP} "rm -rf ${TARGET_BUILD_FILEPATH}"
+                        scp -r build ${TARGET_IP}@${TARGET_IP}:${TARGET_BUILD_FILEPATH}
+                        ssh ${TARGET_ID}@${TARGET_IP} "chmod -R 755 ${TARGET_BUILD_FILEPATH}"
+                        """
                 }
             }
         }
