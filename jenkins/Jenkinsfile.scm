a44cb4a461e1291aa2fa3565ba2c0903caa638ef
eabell <cjp975@naver.com>
2024-04-12 10:58:10 +0900

    test

--- jenkins/Jenkinsfile.scm
+++ jenkins/Jenkinsfile.scm
@@ -2,18 +2,26 @@ pipeline {
     agent any
 
     environment {
-        GIT_URL = 'https://github.com/Ea-Bell/testGItJenkinsTest.git'
-        GIT_BRANCH= 'main'
-        CREDENTIALSID= 'rayful'
-        TARGET_IP='192.168.10.173'
-        TARGET_ID='EaBell'
-        TARGET_BUILD_FILEPATH='/home/EaBell/temp/build' 
+        //######################젠킨스 설정##############################
+            JENKINS_CREDENTIALSID= 'rayful'
+        //##############################################################
+
+        //######################git 설정################################
+            GIT_URL = 'https://github.com/Ea-Bell/testGItJenkinsTest.git'
+            GIT_BRANCH= 'main'
+        //##############################################################
+
+         //######################ssh 설정################################
+            TARGET_IP='192.168.10.173'
+            TARGET_ID='EaBell'
+            TARGET_BUILD_FILEPATH='/home/EaBell/temp/build' 
+        //##############################################################
     }
     stages {
         stage('GitHub Repository Clone') {
             steps {
                 git branch: env.GIT_BRANCH,
-                    credentialsId: env.CREDENTIALSID,
+                    credentialsId: env.JENKINS_CREDENTIALSID,
                     url: env.GIT_URL
             }
         }
@@ -30,9 +38,9 @@ pipeline {
             steps{
                 echo 'send builFile jenkins -> targetServer'
                 dir('my-app'){
-                    bat "ssh ${env.TARGET_ID}@${env.TARGET_IP} 'rm -rf ${env.TARGET_BUILD_FILEPATH}'"
+                    bat "ssh ${env.TARGET_ID}@${env.TARGET_IP} '"rm -rf ${env.TARGET_BUILD_FILEPATH}"'"
                     bat "scp -r build ${env.TARGET_ID}@${env.TARGET_IP}:${env.TARGET_BUILD_FILEPATH}"
-                    bat "ssh ${env.TARGET_ID}@${env.TARGET_IP} 'chmod -R 755 ${env.TARGET_BUILD_FILEPATH}'"
+                    bat "ssh ${env.TARGET_ID}@${env.TARGET_IP} '"chmod -R 755 ${env.TARGET_BUILD_FILEPATH}"'"
                 }
             }
         }
