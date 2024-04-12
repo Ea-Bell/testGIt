2bcf4c4b7bccc8b7033bc9af8d653ff7ed8b18dd
eabell <cjp975@naver.com>
2024-04-12 10:11:23 +0900

    젠킨스 테스트

--- jenkins/Jenkinsfile.scm
+++ jenkins/Jenkinsfile.scm
@@ -35,27 +35,27 @@ pipeline {
                 }
             }
         }
-        stage('SSH transfer') {
-            steps{
-                script {
-                    sshPublisher(
-                    continueOnError: false, failOnError: true,
-                    publishers: [
-                        sshPublisherDesc(
-                        configName: "${env.SSH_CONFIG_NAME}",
-                        verbose: true,
-                        transfers: [
-                            sshTransfer(
-                            sourceFiles: "my-app/build/*", // 전송할 파일 경로 설정
-                            removePrefix: "my-app/build", // 제거할 접두사 설정 (옵션)
-                            remoteDirectory: "/home/EaBell/temp", // 원격 디렉토리 경로 설정
-                            execCommand: "run commands after copy?"
-                            )
-                        ])
-                    ])
-                }
-            }
-        }
+        // stage('SSH transfer') {
+        //     steps{
+        //         script {
+        //             sshPublisher(
+        //             continueOnError: false, failOnError: true,
+        //             publishers: [
+        //                 sshPublisherDesc(
+        //                 configName: "${env.SSH_CONFIG_NAME}",
+        //                 verbose: true,
+        //                 transfers: [
+        //                     sshTransfer(
+        //                     sourceFiles: "my-app/build/*", // 전송할 파일 경로 설정
+        //                     removePrefix: "my-app/build", // 제거할 접두사 설정 (옵션)
+        //                     remoteDirectory: "/home/EaBell/temp", // 원격 디렉토리 경로 설정
+        //                     execCommand: "run commands after copy?"
+        //                     )
+        //                 ])
+        //             ])
+        //         }
+        //     }
+        // }
         // stage('send buildFile'){
         //     steps{
         //         echo 'send builFile jenkins -> targetServer'
