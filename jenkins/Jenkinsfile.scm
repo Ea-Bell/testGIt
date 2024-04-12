d61d51d955ba14021b0cc8f41b1f7d1f950d1818
eabell <cjp975@naver.com>
2024-04-12 10:50:02 +0900

    test

--- jenkins/Jenkinsfile.scm
+++ jenkins/Jenkinsfile.scm
@@ -1,43 +1,38 @@
 pipeline {
     agent any
+
+    environment {
+        GIT_URL = 'https://github.com/Ea-Bell/testGItJenkinsTest.git'
+        GIT_BRANCH= 'main'
+        CREDENTIALSID= 'rayful'
+        TARGET_IP='192.168.10.173'
+        TARGET_ID='EaBell'
+        TARGET_BUILD_FILEPATH='/home/EaBell/temp/build' 
+    }
     stages {
         stage('GitHub Repository Clone') {
             steps {
-                git branch: 'main',
-                    credentialsId: 'rayful',
-                    url: 'https://github.com/Ea-Bell/testGItJenkinsTest.git'
-            }
-        }
-        stage('npm install'){
-            steps {
-                echo 'install'
-                 dir('my-app'){
-                    bat 'npm install'
-                 }
+                git branch: ${GIT_BRANCH},
+                    credentialsId: ${CREDENTIALSID},
+                    url: ${GIT_URL}
             }
         }
         stage('npm build'){
             steps {
                 echo 'buildteset'
                  dir('my-app'){
+                    bat 'npm install'
                     bat 'npm run build'
                  }
             }
         }
-        stage('before remove buildFile'){
-            steps{
-                echo 'test'
-                dir('my-app'){
-                    bat 'ssh EaBell@192.168.10.173 "rm -rf /home/EaBell/temp/build"'
-                }
-            }
-        }
-        stage('send buildFile'){
+        stage('Deploy'){
             steps{
                 echo 'send builFile jenkins -> targetServer'
                 dir('my-app'){
-                    bat 'scp -r build EaBell@192.168.10.173:~/temp'
-                    bat 'ssh EaBell@192.168.10.173 "chmod -R 755 /home/EaBell/temp/build"'
+                    bat 'ssh '{TARGET_ID}'@'${TARGET_IP}' "rm -rf '${TARGET_BUILD_FILEPATH}'"'
+                    bat 'scp -r build '{TARGET_ID}'@'${TARGET_IP}':'${TARGET_BUILD_FILEPATH}''
+                    bat 'ssh '{TARGET_ID}'@'${TARGET_IP}' "chmod -R 755 '${TARGET_BUILD_FILEPATH}'"'
                 }
             }
         }
